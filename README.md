a487fab7a953533ad5992c964bf8a75989fe98bc
Ivan Shiller <ivan.shiller@protonmail.com>
2022-03-31 16:31:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Counter Web Interface
+
+This mini-app allows to easily count positive and negative cases and compute the ratio. It aims to simplify the process, that you can do with the paper and pen. Results are saved in the local storage and the short formated output can be copied to clipboard in one click.
---
+++ script.js
@@ -0,0 +1,86 @@
+const plusC = document.querySelector('#plus-c');
+const minusC = document.querySelector('#minus-c');
+const plusF = document.querySelector('#plus-f');
+const minusF = document.querySelector('#minus-f');
+
+const inputC = document.querySelector('#correct');
+const inputF = document.querySelector('#false');
+const inputT = document.querySelector('#total');
+const inputR = document.querySelector('#rat');
+
+const ratio = document.querySelector('.ratio');
+
+const refresh = document.querySelector('#refresh');
+const copy = document.querySelector('#copy');
+const notif = document.querySelector('#notification');
+
+const handleValueChange = () => {
+    if (parseInt(inputT.value) === 0) {
+        ratio.classList.remove('active');
+    } else {
+        let a = 100 * parseInt(inputC.value) / parseInt(inputT.value);
+        inputR.value = a.toFixed(2) + '%';
+        ratio.classList.add('active');
+    }
+    localStorage.setItem('COUNTIT-correct', inputC.value);
+    localStorage.setItem('COUNTIT-false', inputF.value);
+    localStorage.setItem('COUNTIT-total', inputT.value);
+    localStorage.setItem('COUNTIT-ratio', inputR.value);
+}
+
+const showNotification = (msg) => {
+    notif.classList.add('show');
+    notif.innerHTML = msg;
+    setTimeout(() => {
+        notif.classList.remove('show');
+    }, 2000);
+}
+
+
+inputC.value = localStorage.getItem('COUNTIT-correct') || 0;
+inputF.value = localStorage.getItem('COUNTIT-false') || 0;
+inputT.value = localStorage.getItem('COUNTIT-total') || 0;
+inputR.value = localStorage.getItem('COUNTIT-ratio') || 0;
+handleValueChange();
+
+plusC.addEventListener('click', () => {
+    inputC.value = parseInt(inputC.value) + 1;
+    inputT.value = parseInt(inputT.value) + 1;
+    handleValueChange();
+});
+
+minusC.addEventListener('click', () => {
+    if (parseInt(inputC.value) > 0 && parseInt(inputT.value) > 0) {
+        inputC.value = parseInt(inputC.value) - 1;
+        inputT.value = parseInt(inputT.value) - 1;
+        handleValueChange();
+    }
+});
+
+plusF.addEventListener('click', () => {
+    inputF.value = parseInt(inputF.value) + 1;
+    inputT.value = parseInt(inputT.value) + 1;
+    handleValueChange();
+});
+
+minusF.addEventListener('click', () => {
+    if (parseInt(inputF.value) > 0 && parseInt(inputT.value) > 0) {
+        inputF.value = parseInt(inputF.value) - 1;
+        inputT.value = parseInt(inputT.value) - 1;
+        handleValueChange();
+    }
+});
+
+refresh.addEventListener('click', () => {
+    inputC.value = 0;
+    inputF.value = 0;
+    inputT.value = 0;
+    inputR.value = 0;
+    handleValueChange();
+})
+
+// copy the text from inputR to clipboard on click
+copy.addEventListener('click', () => {
+    navigator.clipboard.writeText(`Total: ${inputT.value} | Correct: ${inputC.value} | False: ${inputF.value} | Ratio: ${inputR.value}`);
+    showNotification('<i class="fas fa-check-circle"></i>&nbsp;&nbsp;Data copied to clipboard!');
+});
